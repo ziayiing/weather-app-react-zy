e44a25922513071e0c3d941ac7b22c66f240a227
ziayiing <ziayiing86@gmail.com>
2023-07-25 17:09:18 +0800

    added weather search

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import React from "react";
+import Weather from "./WeatherSearch";
+import "./App.css";
 
-import './App.css';
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-     Hello 
-      </header>
+      <div className="container">
+        <Weather defaultCity="Kota Kinabalu" />
+        <footer>
+          This project was coded by {""}
+          <a
+            href="https://www.ziayiing86@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Ling Zia Yiing
+          </a>
+          {""} and is {""}
+          open sourced on {""}
+          <a
+            href="https://github.com/ziayiing/weather-app-react-zy"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/WeatherSearch.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
+import "bootstrap/dist/css/bootstrap.min.css";
+import WeatherForecast from "./WeatherForecast";
+import axios from "axios";
+import "./WeatherSearch.css";
+
+export default function WeatherSearch(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  function search() {
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    console.log(apiUrl);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form className="search" onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
+}
